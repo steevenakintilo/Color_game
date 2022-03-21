7590a488da9312e80cde2a6d984e8c33621730ff
steevenakintilo <72024403+steevenakintilo@users.noreply.github.com>
2022-03-21 22:17:58 +0100

    Clean the code

--- main.py
+++ main.py
@@ -31,14 +31,12 @@ def print_file(path):
     return(content)
     f.close()
 
-def split(word):
-    return [char for char in word]
-
 def list_to_str(l):
     s = ""
     for i in range(len(l)):
         s = s + l[i]
     return (s)
+
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -54,7 +52,6 @@ SCREEN_HEIGHT = 1280
 
 
 def menu_loop():
-
     system("clear")
     pygame.init()
     pygame.mixer.init()
@@ -96,25 +93,19 @@ def menu_loop():
             if pressed_keys[ord(' ')]:
                 game_loop()
         screen.fill(color)
-        
         GAME_FONT1.render_to(screen, (800,100) , "COLOR GAME", color_list[rdm_num_one])
         GAME_FONT1.render_to(screen, (700,250) , "PRESS SPACE TO START", white)
         GAME_FONT1.render_to(screen, (0,900) , "Press X to quit ", white)
-        
         pygame.display.flip()
         clock.tick(30)
 
 def game_loop():
-
     system("clear")
     pygame.init()
     pygame.mixer.init()
-    
     GAME_FONT1 = pygame.freetype.Font("font.ttf", 75)
     GAME_FONT2 = pygame.freetype.Font("font.ttf", 200)
-    
     all_collor = ["purple","blue","green","yellow","pink","brown","white","grey","black","orange"]
-    
     purple = (191,64,191)
     blue =  (0,0,255)
     green = (0,128,0)
@@ -125,7 +116,6 @@ def game_loop():
     grey = (204,209,209)
     black = (0,0,0)
     orange = (255,165,0)
-
     score = 0
     color_list = [purple,blue,green,yellow,pink,brown,white,grey,black,orange]
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -162,7 +152,6 @@ def game_loop():
             idx = idx + 1
         if idx % 30 == 0 and time > 0:
             time = time - 1
-        print(idx,time)
         pressed_keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == KEYDOWN:
@@ -180,7 +169,6 @@ def game_loop():
                 name = []
             if pressed_keys[ord('x')]:
                 quit()
-            
             if pressed_keys[ord(' ')] and time <= 0 and idx > 120:
                 game_loop()
             if pressed_keys[ord(' ')] and time > 0:
@@ -193,15 +181,13 @@ def game_loop():
             for i in range(26):
                 if pressed_keys[ord(l[i])]:
                     name.append(l[i])
-                    #print(name)
         screen.fill(color)
         if time > 0:
             GAME_FONT2.render_to(screen, (825 - (len(all_collor[rdm_num_one]) * 25), 30), all_collor[rdm_num_one], color_list[rdm_num_two])
             GAME_FONT1.render_to(screen, (0,0) , "Score " + str(score), white)
             GAME_FONT1.render_to(screen, (0,75) , "Highcore " + str(high), white)
             GAME_FONT1.render_to(screen, (0,150) , "Time left " + str(time), white)
-            GAME_FONT1.render_to(screen, (0,900) , "Press X to quit ", white)
-            
+            GAME_FONT1.render_to(screen, (0,900) , "Press X to quit ", white)    
         else:
             GAME_FONT1.render_to(screen, (800,150) , "Times Up " + str(time), white)
             GAME_FONT1.render_to(screen, (650,650) , "Press space to restart and X to quit", white)
@@ -212,12 +198,5 @@ def game_loop():
                 GAME_FONT2.render_to(screen, (pos[i]+100, 530), name[i], (30, 30, 30))
         pygame.display.flip()
         clock.tick(30)
-def listToString(s): 
-    
-    # initialize an empty string
-    str1 = " " 
-    
-    # return string  
-    return (str1.join(s))
 
 menu_loop()
